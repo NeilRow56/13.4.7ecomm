65f839e1ac6c8636b5af11b6b4c63aba97ebfc2a
Neil Rowland <neilrowland56@gmail.com>
2023-07-01 17:15:48 +0100

    Added api route to create store

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -16,6 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -23,6 +24,8 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const [loading, setLoding] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -55,17 +58,27 @@ export const StoreModal = () => {
                     <FormItem>
                       <FormLabel>Name</FormLabel>
                       <FormControl>
-                        <Input placeholder="E-Commerce" {...field} />
+                        <Input
+                          disabled={loading}
+                          placeholder="E-Commerce"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
                 <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                  <Button variant="outline" onClick={storeModal.onClose}>
+                  <Button
+                    disabled={loading}
+                    variant="outline"
+                    onClick={storeModal.onClose}
+                  >
                     Cancel
                   </Button>
-                  <Button type="submit">Continue</Button>
+                  <Button disabled={loading} type="submit">
+                    Continue
+                  </Button>
                 </div>
               </form>
             </Form>
