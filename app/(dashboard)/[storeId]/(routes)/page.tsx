50cecf9b4035c335c659c258bdf38d444c356b0b
Neil Rowland <neilrowland56@gmail.com>
2023-07-02 09:45:41 +0100

    Added name of active store

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,17 @@
+import prismadb from "@/lib/prismadb";
 import { FC } from "react";
 
-interface DashboardPageProps {}
+interface DashboardPageProps {
+  params: { storeId: string };
+}
 
-const DashboardPage: FC<DashboardPageProps> = ({}) => {
-  return <div>Dashboard page</div>;
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active Store: {store?.name}</div>;
 };
 
 export default DashboardPage;
