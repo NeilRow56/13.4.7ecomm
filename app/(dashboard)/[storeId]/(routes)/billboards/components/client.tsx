0f83d14d9db9bb7105fdb240e576dd795908ace2
Neil Rowland <neilrowland56@gmail.com>
2023-07-07 12:01:38 +0100

    Data Table and API list compleyed

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator";
 
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -32,6 +33,9 @@ const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API calls for billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardsId" />
     </>
   );
 };
